ad3f7dd2a4b1453e7e35a9855c6e40dddb9797c7
yichair <996178163@qq.com>
2021-12-07 18:16:10 +0800

    new dataset

---
+++ dataset.py
@@ -0,0 +1,46 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.backends.cudnn as cudnn
+from torch.utils.tensorboard import SummaryWriter
+import torchvision
+import torchvision.transforms as transforms
+import os
+import argparse
+from utils import progress_bar
+from model import Model
+import trainer
+import math
+import numpy
+import matplotlib.pyplot as plt
+
+transform_test = transforms.Compose([
+    transforms.Resize((150, 150)),
+    transforms.ToTensor(),
+])
+
+testset = torchvision.datasets.ImageFolder(
+    root='/home/skye/intel-image/seg_test', transform=transform_test)
+testloader = torch.utils.data.DataLoader(
+    testset, batch_size=1, shuffle=False, num_workers=1)
+
+classes = ["buildings", "forest", "glacier", "mountain", "sea", "street"]
+
+index = 0
+
+for batch_idx, (inputs, targets) in enumerate(testloader):
+    for image in inputs:
+        image = image.unsqueeze(0)
+        target = classes[targets[0]]
+        numpy.save(f"{target}/{index}.npy",image.numpy())
+        index += 1
+
+predset = torchvision.datasets.ImageFolder(
+    root='/home/skye/intel-image/seg_pred', transform=transform_test)
+
+for item_index, (t,target) in enumerate(predset):
+    path,_ = predset.imgs[item_index]
+    t = t.unsqueeze(0)
+    filename = os.path.basename(path).split('.')[0]
+    numpy.save(f"unknown/{filename}.npy",t.numpy())
---
+++ datasets/data.py
@@ -0,0 +1,32 @@
+# by yi 数据集
+from datasets.config import *
+from datasets.data_loader import *
+from easydl import *
+from collections import Counter
+from torchvision.transforms.transforms import *
+from torch.utils.data import DataLoader
+from torch.utils.data.sampler import WeightedRandomSampler
+
+source_classes = [i for i in range(args.datasets.n_classes)]
+target_classes = [i for i in range(args.datasets.n_classes)]
+target_aux_classes = [i for i in range(args.datasets.aux_classes + 1)]
+
+train_transform = Compose([
+    Resize(256),
+    RandomCrop(224),
+    RandomHorizontalFlip(),
+    ToTensor(),
+])
+
+
+test_transform = Compose([
+    Resize(256),
+    CenterCrop(224),
+    ToTensor(),
+])
+
+# by yi self supervision target data and test data
+target_train_dl = get_target_dataloader(args.datasets.tar)  # 训练数据集
+target_pseudo_label_dl = get_pseudo_label_dataloader(args.datasets.tar)  # 带旋转标签的数据集
+target_test_dl = get_test_dataloader(args.datasets.test) # 测试数据集
+_, target_val_loader = get_train_val_dataloader(args.datasets.tar)
